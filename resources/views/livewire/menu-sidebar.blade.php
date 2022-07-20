ef0f7e936c6a22133e871850c05fda9253a58f08
catarinacci <catarinacci@gmail.com>
2022-07-20 13:46:53 -0300

    hover menu

--- resources/views/livewire/menu-sidebar.blade.php
+++ resources/views/livewire/menu-sidebar.blade.php
@@ -10,8 +10,8 @@
     <div x-on:click.away="open=false" class="bg-indigo-900 sticky top-0">
         <div class="container flex items-center h-20 justify-between ">
             {{-- <a x-on:click="$ref.sidebar.classList.remove('hidden')"> --}}
-            <a x-on:click="open = !open" class="hover:text-indigo-400">
-                <div class="flex flex-col items-center justify-center  text-green-400 cursor-pointer  font-semibold" href="">
+            <a x-on:click="open = !open" >
+                <div class="flex flex-col items-center justify-center hover:text-indigo-400 text-green-400 cursor-pointer  font-semibold" >
                     <svg class="h-10 w-10  " stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
@@ -31,7 +31,7 @@
                 <img class="w-50 h-20" src="https://note-api-catarinacci.s3.sa-east-1.amazonaws.com/noteapi/Free_Sample_By_Wix(4).jpg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                 </a>
             </div>
-                <div class="flex flex-col items-center order-last justify-center text-green-400 cursor-pointer rounded-lg hover:bg-indigo-400 font-semibold ">
+                <div class="flex flex-col items-center order-last justify-center text-green-400 cursor-pointer rounded-lg hover:text-indigo-400 font-semibold ">
                     <button class="outline outline-offset-4 outline-1 outline-indigo-500 rounded-lg   ">DEJA TU <br>COMENTARIO</button>
                 </div>
 
@@ -44,7 +44,7 @@
             {{-- x-show = "open" --}}
             {{-- :class="{'block' : open, 'hidden' : !open }" --}}
             :class="{'block' : open, 'hidden' : !open }"
-            class=" fixed top-0 bottom-0 lg:left-0 p-2 overflow-y-auto transition duration-700 ease-in w-80  text-center bg-gray-900 hidden ">
+            class=" fixed top-0 bottom-0 lg:left-0 p-2 overflow-y-auto transition duration-700 ease-in w-80 text-center bg-gray-900 hidden ">
 
             <div >
                 <div class="grid grid-cols-3 items-center ">
