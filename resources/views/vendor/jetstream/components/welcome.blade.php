97363b755e304c3894a2b1b611dfc31ecb8b286b
catarinacci <catarinacci@gmail.com>
2022-10-19 17:28:43 -0300

    imagen view home responsive

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,4 +1,4 @@
-<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+<div class="p-6 sm:px-20 sm:rounded-lg bg-white border-b border-gray-200">
     {{-- <div>
         <x-jet-application-logo class="block h-12 w-auto" />
     </div> --}}
@@ -18,7 +18,7 @@
 </div>
 
 <div class="bg-gray-200 bg-opacity-25 ">
-     <div class="p-6">
+     <div class="p-6 overflow-hidden">
         <div class="flex items-center">
             {{-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-400"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg> --}}
             <div class="text-gray-600">
@@ -30,8 +30,27 @@
         <div class="ml-4 text-sm text-gray-600 leading-7 font-semibold">
             Las pruebas son realizadas con Postman, en la pestaña Headers colocamos KEY: Accept y VALUE: application/json.
         </div>
-        <div class="">
-            <img class="" src= "https://note-api-catarinacci.s3.sa-east-1.amazonaws.com/noteapi/home_config.jpg" alt="">
+        <div class="size_p mt-5 overflow-y-auto overflow-x-auto">
+            <div class="size  ">
+                <img class="" src= "https://note-api-catarinacci.s3.sa-east-1.amazonaws.com/noteapi/home_config.jpg" alt="">
+            </div>
         </div>
     </div>
 </div>
+
+<style>
+.size{
+    height: 250px;
+    width: 800px;
+
+}
+.size_sm{
+    height: 300px;
+    width: 500px
+}
+.size_p{
+    height: 250px;
+    width: 550px;
+
+}
+</style>
