c4eded5e21c862e5e54c5fdc7cba3de4185563f1
catarinacci <catarinacci@gmail.com>
2022-10-23 16:49:35 -0300

    modify footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="p-4 flex justify-center items-center bg-white  dark:bg-gray-800">
+<footer class="p-4 flex justify-center items-center bg-white">
     <div class=" ">
         <a href="#" target="_blank" class="flex items-center">
             <img src="https://note-api-catarinacci.s3.sa-east-1.amazonaws.com/noteapi/logo_fondo_blanco.jpg" class="mr-4 h-10" alt="FlowBite Logo">
@@ -6,6 +6,6 @@
         </a>
     </div>
     <div class="">
-        <span class="text-sm ml-20 text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a target="_blank" class="hover:underline">NoteAPI™</a>. All Rights Reserved.</span>
+        <span class="text-sm ml-3 sm:ml-20 text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a target="_blank" class="hover:underline">NoteAPI™</a>. All Rights Reserved.</span>
     </div>
 </footer>
