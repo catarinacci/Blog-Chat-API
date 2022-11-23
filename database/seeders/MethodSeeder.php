d974c453730bb9bad8fd9c567e1828164f080244
catarinacci <catarinacci@gmail.com>
2022-11-23 11:38:35 -0300

    module category and tag

--- database/seeders/MethodSeeder.php
+++ database/seeders/MethodSeeder.php
@@ -53,63 +53,88 @@ class MethodSeeder extends Seeder
             ],
             [
                 'module_id' => '2',
+                'name' => 'Type_Categories'
+            ],
+            [
+                'module_id' => '2',
+                'name' => 'Search'
+            ],
+            [
+                'module_id' => '3',
+                'name' => 'Type_Tags'
+            ],
+            [
+                'module_id' => '3',
+                'name' => 'Search'
+            ],
+
+            [
+                'module_id' => '4',
                 'name' => 'All_Notes'
             ],
             [
-                'module_id' => '2',
+                'module_id' => '4',
                 'name' => 'Create_Note'
             ],
             [
-                'module_id' => '2',
+                'module_id' => '4',
                 'name' => 'Select_Note'
             ],
             [
-                'module_id' => '2',
+                'module_id' => '4',
                 'name' => 'Update_Note'
             ],
             [
-                'module_id' => '2',
+                'module_id' => '4',
                 'name' => 'Delete_Note'
             ],
             [
-                'module_id' => '2',
+                'module_id' => '4',
                 'name' => 'Search_Notes'
             ],
             [
-                'module_id' => '2',
+                'module_id' => '4',
                 'name' => 'My_Notes'
             ],
             [
-                'module_id' => '3',
+                'module_id' => '5',
                 'name' => 'Create_Comment'
             ],
             [
-                'module_id' => '3',
+                'module_id' => '5',
                 'name' => 'Delete_Comment'
             ],
             [
-                'module_id' => '4',
+                'module_id' => '6',
                 'name' => 'To_Note'
             ],
             [
-                'module_id' => '4',
+                'module_id' => '6',
                 'name' => 'To_Comment'
             ],
             [
-                'module_id' => '4',
+                'module_id' => '6',
                 'name' => 'Delete_Reaction'
             ],
             [
-                'module_id' => '5',
+                'module_id' => '6',
+                'name' => 'Type_Reaction'
+            ],
+            [
+                'module_id' => '7',
                 'name' => 'My_Notifications'
             ],
             [
-                'module_id' => '5',
+                'module_id' => '7',
                 'name' => 'Unread'
             ],
             [
-                'module_id' => '5',
+                'module_id' => '7',
                 'name' => 'Read'
+            ],
+            [
+                'module_id' => '7',
+                'name' => 'Mask_As_Read'
             ],
         ];
 
